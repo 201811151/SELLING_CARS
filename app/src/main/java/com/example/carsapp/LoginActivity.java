edeadf3774b9ed59791b62e5c7600c0a740047e8
qsa <201811151@gcet.edu.om>
2023-05-31 08:36:35 -0700

    SELLING_CARS

---
+++ app/src/main/java/com/example/carsapp/LoginActivity.java
@@ -0,0 +1,42 @@
+package com.example.carsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText UserName;
+    EditText password;
+    Button button1;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        UserName = findViewById(R.id.UserName);
+        password = findViewById(R.id.password);
+        button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (UserName.getText().toString().equals("User") && password.getText().toString().equals("1234")) {
+                    Intent intent =new Intent(LoginActivity.this,HomeActivity.class );
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(LoginActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+
+    }
+}
